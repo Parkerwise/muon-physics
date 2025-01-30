b14c01d0a1f7cb5cfa3d39d8e8cf85fdea2c010b
Parkerwise <wiseparker03@gmail.com>
2025-01-30 14:06:50 -0600

    added fitting

--- python/histogram-fit.py
+++ python/histogram-fit.py
@@ -23,23 +23,31 @@ for decay_time in valid_times_ns:
 lists = sorted(occurences.items())
 decay_time, muon_count = zip(*lists)
 
-muon_count = len(valid_times_ns)
+total_muon_count = len(valid_times_ns)
 rate_guess = 4.54e-4
-guess = [muon_count, rate_guess]
-parameters, covariance = curve_fit(decay_power_law, decay_time,
-                                   muon_count, p0=guess)
+guess = [total_muon_count, rate_guess]
+parameters, covariance = curve_fit(f=decay_power_law, xdata=decay_time,
+                                   ydata=muon_count, p0=guess)
+
+model_total = parameters[0]
+model_decay_rate = parameters[1]
+model_muon_count = []
+for time in decay_time:
+    entry = decay_power_law(time, model_total, model_decay_rate)
+    model_muon_count.append(entry)
+
+# real_model_muon_count = []
+# for time in decay_time:
+#     entry = decay_power_law(time, total_muon_count, rate_guess)
+#     real_model_muon_count.append(entry)
 
 # plotting
 plt.plot(decay_time, muon_count, "o")
+plt.plot(decay_time, model_muon_count, color="red")
+# plt.plot(decay_time, real_model_muon_count, color="green")
 plt.xlabel("Muon decay time")
 plt.ylabel("Events")
-plt.savefig("muon-histogram.pdf")
-# print(len(foo))
-# print(len(valid_times_ns))
+plt.show()
+# plt.savefig("muon-histogram.pdf")
 # plt.hist(valid_times_ns, bins=1)
 # plt.show()
-
-
-# import data
-# fit data
-# plot fit
